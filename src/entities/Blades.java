3e2f6bfa397a2cb3a1c096a9f876cabf31e9f036
lorenzo-scatena <85491491+lorenzo-scatena@users.noreply.github.com>
2024-10-17 20:42:13 +0200

    0.0.3

--- src/entities/Blades.java
+++ src/entities/Blades.java
@@ -3,7 +3,7 @@ package entities;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import java.util.Random;
 import static utils.Constants.Entities.ENTITY_SIZE;
 import static utils.Constants.GameWindow.WINDOW_HEIGHT;
 import static utils.Constants.GameWindow.WINDOW_WIDTH;
@@ -14,12 +14,16 @@ import javax.imageio.ImageIO;
 public class Blades extends Entity{
 
     private BufferedImage [] redFrames;
-    private int SPEED = 2;
-
+    private double SPEED_Y = 2;
+    private double SPEED_X;
+    private Random rand;
     public Blades() {
-        super();
-        this.x = WINDOW_WIDTH / 2 - ENTITY_SIZE / 2; // centers the player in the middle of the panel
-        this.y = WINDOW_HEIGHT - ENTITY_SIZE;
+        this.rand= new Random();
+        this.SPEED_X= 5;
+        this.x=x;
+        this.y=y;
+        //this.x = WINDOW_WIDTH / 2 - ENTITY_SIZE / 2; // centers the player in the middle of the panel
+        //this.y = WINDOW_HEIGHT - ENTITY_SIZE;
         // Load sprite sheet
         try {
             spriteSheet = ImageIO.read(new File("src/img/red-blade.png"));
@@ -34,7 +38,18 @@ public class Blades extends Entity{
 
     public void update(){
         frameCount++;
+        x+=SPEED_X;
+        y+=SPEED_Y;
+        SPEED_Y+=0.1;
+        if (y>=600) { // 
+             // Ripristina la posizione
+            SPEED_Y = -SPEED_Y * 0.8; // Inverti la velocità e applica un coefficiente di restituzione
+        }
 
+        // Controllo dei bordi
+        if (x < 0 || x > 535) {
+            SPEED_X = -SPEED_X; // Rimbalzo orizzontale
+        }
         if (frameCount >= frameDelay) {
             currentFrame++;
             frameCount = 0; // Resets the frame counter
